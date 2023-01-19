8e20c2dbac3dde405705cec67bbaaf6298191c97
Yanina Amatore <yaninaamatore@gmail.com>
2023-01-19 14:36:45 +1300

    Complete task feature working

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {FC,ChangeEvent, useState} from 'react';
 import {ITask} from './interfaces'
 
 import './App.css';
+import TodoTask from './Components/TodoTask';
 
 const App:FC =()=> {
 
@@ -9,31 +10,44 @@ const App:FC =()=> {
  const [deadline, setDeadline] = useState<number>(0)
  const [todoList, setTodoList] = useState<ITask[]>([])
 
- const handleChange = (event:ChangeEvent <HTMLInputElement>):void => {
-    if (event.target.name === 'task'){
-      setTask(event.target.value)
-    }else{
-      setDeadline(Number(event.target.value))
-    }
- }
+ const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  if (event.target.name === "task") {
+    setTask(event.target.value);
+  } else if(event.target.name === "deadline") {
+    setDeadline(Number(event.target.value));
+  }
+};
 
  const addTask = ():void => {
   const newTask = {taskName:task, deadline: deadline}
   setTodoList([...todoList, newTask])
   setTask("")
   setDeadline(0)
+  console.log(deadline)
  }
 
+ const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    )
+  }
+  
   return (
    <div className='App'>
     <div className='header'>
       <div className='inputContainer'>
         <input type='text' placeholder='Task ...' name='task' onChange={handleChange}/>
-        <input type='number' placeholder='Deadline (in days) ...'name='deadline'/>
+        <input type="number" placeholder='Deadline (in days) ...' name='deadline'/>
       </div>
       <button onClick={addTask} >Add Task</button>
     </div>
-    <div className='todoList'></div>
+    <div className='todoList'>
+    {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+    </div>
    </div>
  )
 }
